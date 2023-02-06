60b92d01ee9a6e3ef4897f656f07bbcb60d74ad9
Dmitrii Chich <86652230+mrdamos69@users.noreply.github.com>
2023-02-06 07:14:11 -0500

    up makefile and unit_test

--- unit_test/unit_test.cc
+++ unit_test/unit_test.cc
@@ -1,40 +1,40 @@
-//#include <gtest/gtest.h>
+#include <gtest/gtest.h>
 
-//#include "../controller/controller.h"
-//#include "../model/agents/lib_agents.h"
+#include "../controller/controller.h"
+#include "../model/agents/lib_agents.h"
 
-//TEST(Test_Monitoring_system, test_cpu_agent) {
-// s21::Controller controller;
-// controller.starting_cpu_agent();
-//}
+TEST(Test_Monitoring_system, test_cpu_agent) {
+    s21::Controller controller;
+    controller.starting_cpu_agent();
+}
 
-//TEST(Test_Monitoring_system, test_memory_agent) {
-// s21::Controller controller;
-// controller.starting_memory_agent();
-//}
+TEST(Test_Monitoring_system, test_memory_agent) {
+    s21::Controller controller;
+    controller.starting_memory_agent();
+}
 
-//TEST(Test_Monitoring_system, test_network_agent) {
-// s21::Controller controller;
-// std::string url_ = "https://edu.21-school.ru";
-// controller.starting_network_agent(url_);
-//}
+TEST(Test_Monitoring_system, test_network_agent) {
+    s21::Controller controller;
+    std::string url_ = "https://edu.21-school.ru";
+    controller.starting_network_agent(url_);
+}
 
-//TEST(Test_Monitoring_system, test_special_agent) {
-// s21::Controller controller;
-// std::string lvl_privilege = "cpu_idle_usage";
-// controller.starting_special_agent(lvl_privilege);
-//}
+TEST(Test_Monitoring_system, test_special_agent) {
+    s21::Controller controller;
+    std::string lvl_privilege = "cpu_idle_usage";
+    controller.starting_special_agent(lvl_privilege);
+}
 
 
-//TEST(Test_Monitoring_system, test_starting_agents) {
+// TEST(Test_Monitoring_system, test_starting_agents) {
 // s21::Controller controller;
 // std::string idle_ = "cpu_idle_usage";
 //  std::string url_ = "https://edu.21-school.ru";
 //  int time_ = 5;
 // controller.starting_agents_thread(idle_, url_, time_); // 5 секунд время работы функции
-//}
+// }
 
-//int main(int argc, char *argv[]) {
-// testing::InitGoogleTest(&argc, argv);
-// return RUN_ALL_TESTS();
-//}
+int main(int argc, char *argv[]) {
+testing::InitGoogleTest(&argc, argv);
+return RUN_ALL_TESTS();
+}
